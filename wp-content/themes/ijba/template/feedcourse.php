a93c8c23a9db10eb72311cde9a07e36d4d4bc19f
Vinicius de Santana <vinicius.vts@gmail.com>
2021-07-06 13:13:37 -0300

    fix: duplicate more courses slide

--- wp-content/themes/ijba/template/feedcourse.php
+++ wp-content/themes/ijba/template/feedcourse.php
@@ -16,7 +16,7 @@
             <!-- conteúdo -->
             <div class="tab-content mt-5" id="pills-tabContent">
                 <div class="tab-pane fade show active" id="pills-extensao" role="tabpanel" aria-labelledby="pills-extensao-tab">
-                    <div class="slider-course">
+                    <div class="slider-course" id="extensao">
                         <div class="container-slider-course">
                             <div class="itens-slider-course">
                                 <?php
@@ -42,8 +42,12 @@
                                         <div class="row">
                                             <div class="col-md-6 slider-description-course">
                                                 <h4>
-                                                    <?php foreach ($categorias_extensao as $categoria_extensao) : echo $categoria_extensao->name;
-                                                    endforeach; ?>
+                                                    <?php
+                                                    foreach ($categorias_extensao as $key => $categoria_extensao) {
+                                                        if($key != 0) echo ' | ';
+                                                        echo $categoria_extensao->name;
+                                                    }
+                                                    ?>
                                                 </h4>
                                                 <h3>
                                                     <?php
@@ -86,11 +90,12 @@
                         </div>
                         <div class="text-center">
                             <div class="navContainer-course">
-                                <div class="navArrow" id-slider="section-feed-courses">
+                                <div class="navArrow" id-slider="extensao">
                                     <button class="navPrev" style="opacity: 0.1" disabled="true">
                                         ‹
                                     </button>
-                                    <button class="navNext">
+                                    <button class="navNext"
+                                    <?php if($positionExtensao <= 1){ echo 'style="opacity: 0.1" disabled="true"';} ?>>
                                         ›
                                     </button>
                                 </div>
@@ -99,26 +104,93 @@
                     </div>
                 </div>
                 <div class="tab-pane fade" id="pills-aperfeicoamento" role="tabpanel" aria-labelledby="pills-aperfeicoamento-tab">
+                    <div class="slider-course" id="aperf">
+                        <div class="container-slider-course">
+                            <div class="itens-slider-course">
+                                <?php
+                                $wp_query_aperf = new WP_Query(array(
+                                    'post_type' => 'curso',
+                                    'posts_per_page' => 6,
+                                    'tax_query' => array(
+                                        array(
+                                            'taxonomy' => 'categoria_curso',
+                                            'field' => 'slug',
+                                            'terms' => 'aperfeicoamento'
+                                        )
+                                    ),
+                                ));
+                                $positionAperf = 0;
+                                
+                                $postsExensao = $wp_query_aperf->found_posts;
+                                
+                                while ($wp_query_aperf->have_posts()) : $wp_query_aperf->the_post();
+                                    $categorias_aperf = wp_get_object_terms($post->ID, 'categoria_curso');
+                                ?>
+                                    <div class="item-slider-course">
+                                        <div class="row">
+                                            <div class="col-md-6 slider-description-course">
+                                                <h4>
+                                                    <?php
+                                                    foreach ($categorias_extensao as $key => $categoria_extensao) {
+                                                        if($key != 0) echo ' | ';
+                                                        echo $categoria_extensao->name;
+                                                    }
+                                                    ?>
+                                                </h4>
+                                                <h3>
+                                                    <?php
+                                                        the_title();
+                                                    ?>
+                                                </h3>
+                                                <p>
+                                                    <?php
+                                                    $justificativa = get_field('justificativa', get_the_id());
+                                                    $justificativaStripTags = strip_tags($justificativa);
+                                                    echo $excerpt = substr($justificativaStripTags, 0, 320);
+                                                    ?>
+                                                    [...]
+                                                </p>
+                                                <a href="<?php the_permalink(); ?>">
+                                                    <button class="green">Saiba mais</button>
+                                                </a>
+                                            </div>
+                                            <div class="col-md-6 slider-description-image">
+                                                <?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php
+                                    $positionAperf++;
+                                endwhile;
+                                ?>
+                            </div>
+                        </div>
+                        <div class="bullets-slider-course">
+                        
+                            <ul>
+                                <?php for ($list = 0; $list < $positionAperf; $list++) { ?>
+                                    <li index-bullet="<?php echo $list ?>" class="<?php if ($list == 0) : echo "active"; endif; ?>"></li>
+                                <?php } ?>
+                                
+                            </ul>
+                        </div>
+                        <div class="text-center">
+                            <div class="navContainer-course">
+                                <div class="navArrow" id-slider="aperf">
+                                    <button class="navPrev" style="opacity: 0.1" disabled="true">
+                                        ‹
+                                    </button>
+                                    <button class="navNext"
+                                    <?php if($positionAperf <= 1){ echo 'style="opacity: 0.1" disabled="true"';} ?>>
+                                        ›
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <?php
-                    $wp_query = new WP_Query(array(
-                        'post_type' => 'curso',
-                        'posts_per_page' => 6,
-                        'tax_query' => array(
-                            array(
-                                'taxonomy' => 'categoria_curso',
-                                'field' => 'slug',
-                                'terms' => 'aperfeicoamento'
-                            )
-                        ),
-                    ));
-                    $position = 0;
-                    // var_dump($wp_query);
-                    while ($wp_query->have_posts()) : $wp_query->the_post();
-                    ?>
-                        <h3><?php the_title(); ?></h3></br>
-                    <?php
-                        $position++;
-                    endwhile;
+                    wp_reset_postdata();
+                    wp_reset_query();
                     ?>
                 </div>
             </div>
